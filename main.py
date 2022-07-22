3dcfd97a389dea9dbf5b3779d45f6c3afa129536
GrimIrani <59345395+GrimIrani@users.noreply.github.com>
2022-07-22 10:12:39 +0000

    Session
    Base of sessions added(some TODO in my mind)

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-#TeleShell Bot v0.03 BETA
+#TeleShell Bot v0.05 BETA
 #Simple shell Executor telegram bot writen with aiogram(Python Lib) 
 from aiogram import Bot, Dispatcher, executor, types
 import os
 
-API_TOKEN = ''
+API_TOKEN = '' #<<< Set your bot's API token Here >>>
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("TeleShell BETA v0.03\nmade with <3")
+    await message.reply("TeleShell BETA v0.05\nmade with <3")
 
 @dp.message_handler()
 async def echo(message: types.Message):
     try:
+        session = await message.answer(message.text+" commend,\n STARTED...❤️")
         result_record = os.system("%s > temp.result" % message.text)
         result = open("temp.result", "r+").read()
-        await message.answer(result)
+        await session.edit_text(result)
     except:
-        await message.answer(message.text+" commend,\n Has failed.")
+        await session.edit_text(message.text+" commend,\n FAILED!💔")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
