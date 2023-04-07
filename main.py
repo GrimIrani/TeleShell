c9aa3bf027c2c4855ce3dd9cfe5c2078934c5f02
GrimIrani <59345395+GrimIrani@users.noreply.github.com>
2023-04-08 00:25:18 +0200

    Add TempFile

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-#TeleShell Bot v0.05 BETA
+#TeleShell Bot v0.06 BETA
 #Simple shell Executor telegram bot writen with aiogram(Python Lib) 
 from aiogram import Bot, Dispatcher, executor, types
 import os
+import tempfile
 
 API_TOKEN = '' #<<< Set your bot's API token Here >>>
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+temp = tempfile.NamedTemporaryFile(prefix="TeleShell_")
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply("TeleShell BETA v0.05\nmade with <3")
+async def welcome(message: types.Message):
+    await message.reply("TeleShell BETA v0.06\nmade with <3")
 
 @dp.message_handler()
 async def echo(message: types.Message):
     try:
         session = await message.answer(message.text+" commend,\n START...<3")
-        result_record = os.system("%s > temp.result" % message.text)
-        result = open("temp.result", "r+").read()
+        result_record = os.system("%s > %s" % message.text, temp.name)
+        result = open(temp.name, "r+").read()
         await session.edit_text(result)
     except:
         await session.edit_text(message.text+" commend,\n FAILED!</3")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)optionoption
