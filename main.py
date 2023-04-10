d7b7b96afc43495a3fe3b19a96e55bf8fcd16ffe
GrimIrani <59345395+GrimIrani@users.noreply.github.com>
2023-04-10 22:51:04 +0300

    Realtime/live answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#TeleShell Bot v0.08 BETA
+#TeleShell Bot v0.09
 #Simple shell Executor telegram bot writen with aiogram(Python Lib) 
 from aiogram import Bot, Dispatcher, executor, types
 import os, sys
@@ -9,21 +9,25 @@ API_TOKEN = '' #<<< Set your bot's API token Here >>>
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-temp = tempfile.NamedTemporaryFile(prefix="TeleShell_")
+temp = tempfile.NamedTemporaryFile(prefix="TeleShell_", mode='w+')
 
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
-    await message.reply("TeleShell BETA v0.08\nmade with <3")
+    await message.reply("TeleShell BETA v0.09\nmade with <3")
 
 @dp.message_handler()
 async def echo(message: types.Message):
     try:
         session = await message.answer(message.text+" commend,\n START...<3")
-        result_record = os.system("%s > %s" % message.text, temp.name)
-        result = open(temp.name, "r+").read()
-        await session.edit_text(result)
+        with os.popen(message.text) as process:
+            for line in process:
+                temp.write(line)
+                temp.seek(0)
+                await session.edit_text(temp.read())
     except:
         await session.edit_text(message.text+" commend,\n FAILED!</3")
+    finally:
+        temp.truncate(0)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
